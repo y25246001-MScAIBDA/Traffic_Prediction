2f4a8984997a493913d22ff3dbe105ed64649442
y25246001-MScAIBDA <y25246001@dhsgsu.edu.in>
2026-05-01 14:37:04 +0530

    Update Traffic_Prediction.py

--- Traffic_Prediction.py
+++ Traffic_Prediction.py
@@ -25,7 +25,7 @@ st.set_page_config(
 # -------------------------------
 # Load Model
 # -------------------------------
-traffic_model = pickle.load(open('traffic_model.sav', 'rb'))
+traffic_model = pickle.load(open('Traffic_model.sav', 'rb'))
 
 # -------------------------------
 # Validation Function
